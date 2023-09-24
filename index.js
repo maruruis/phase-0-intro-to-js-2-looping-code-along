cd9588b8effdc161caecbbd52f46840268933851
Ian Kimani <ianmwng@gmail.com>
2023-09-24 17:39:27 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    
+    let messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  function countDown(start) {
+    for (let i = start; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(4);
+  
